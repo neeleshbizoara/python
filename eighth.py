f15995f2305907c8cf3b3f8dc986231067afb010
neeleshbizoara <bizoaraneelesh@gmail.com>
2019-06-14 11:21:13 +0530

    Errors and Exceptions

---
+++ eighth.py
@@ -0,0 +1,37 @@
+'''import os
+os.getcwd()
+
+#builid finction
+list1 = [1,2,3]
+#print(list1.__mro__())
+
+
+while True:
+    try:
+        x= input('Enter any thing:')
+        print(int(x))
+    except: 
+        pass
+        print('Error found')
+        exit()
+        #break
+        #raise
+    else:
+        print('Else block')
+#ValueError:     # expression as identifier:
+'''
+
+print(NameError.__mro__)
+
+
+class listClass(list):
+    pass
+
+obj1 = listClass()
+print(obj1)
+obj1.append(1)
+print(obj1)
+
+print(listClass.__mro__)
+
+'''finally'''
