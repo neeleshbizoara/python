70943ffe1b00463815bdde28b468936fd860461b
neeleshbizoara <bizoaraneelesh@gmail.com>
2019-06-17 11:47:09 +0530

    Input and Output

---
+++ eighthClass.py
@@ -0,0 +1,37 @@
+'''Read Wirte'''
+#seek()
+#with keyword can use : ?
+'''f = open('test.txt', 'w')
+f.close()
+exit()
+
+modes:
+r - read 
+w - write
+a - append
+r+ - read and append    read and write both
+w+ - write and append   creates a new file / overwrites the file
+
+read()
+readline()
+readlines()
+wirte()
+
+
+seek()
+tell()
+with open("filename", "m") as alias:
+    use alias
+
+    '''
+
+
+'''with open('test.txt', 'w') as f:
+    f.write('This is first line')
+
+exit()
+'''
+#print(x, end='')
+
+
+#Pass by ref and pass by value
